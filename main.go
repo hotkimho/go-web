75559dc7aeafe4ab9623ec6add82966bd2524a9e
hotkimho <rlaghekd777@naver.com>
2022-06-14 02:08:25 +0900

    Test Hadnler

--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/gorilla/mux"
-	_ "go-sql-driver/mysql"
 	"net/http"
 )
 
-func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/book/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		//
-		fmt.Println(r.Cookies())
-	})
-
-	db, err := sql.Open("mysql", "root:!gktrlagh33@(127.0.0.1:3306)/go_user?parseTime=true")
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = db.Ping()
-	http.ListenAndServe(":80", router)
+func testFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "test Function\n")
 }
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello Function\n")
+	})
+	mux.Handle("/test", http.HandlerFunc(testFunc))
+	http.ListenAndServe("127.0.0.1:80", mux)
+}
+
+//
+//import (
+//	"database/sql"
+//	"fmt"
+//	_ "github.com/go-sql-driver/mysql"
+//	"time"
+//)
+//
+//var databaseURL string = "root:!gktrlagh33@(127.0.0.1:3306)/go_user?parseTime=true"
+//
+//func main() {
+//
+//	db, err := sql.Open("mysql", databaseURL)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	username := "kimho"
+//	password := "rlagh33"
+//	createAt := time.Now()
+//
+//	result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createAt)
+//	if err != nil {
+//		fmt.Println(err)
+//	}
+//	userID, err := result.LastInsertId()
+//	fmt.Println(userID)
+//}
